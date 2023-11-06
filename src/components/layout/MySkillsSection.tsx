f4a952b5dc58cad04b677443cd5bb9a13f847c71
PatrickAsiedu <pattribette@gmail.com>
2023-11-06 15:08:25 +0000

    tailwind setup

---
+++ src/components/layout/MySkillsSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MySkillsSection = () => {
+  return (
+    <div>MySkillsSection</div>
+  )
+}
+
+export default MySkillsSection
---
+++ src/components/layout/MyWorkSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyWorkSection = () => {
+  return (
+    <div>MyWorkSection</div>
+  )
+}
+
+export default MyWorkSection
